ab9af49d5bccbc0f005bf37e0acae38f14636dc5
lvsouza <ls18017@gmail.com>
2020-11-18 23:10:05 -0300

    refactor(main.ts): removed unnused code

--- electron/main.ts
+++ electron/main.ts
@@ -1,17 +1,10 @@
-import {
-  app,
-  Menu,
-  shell,
-  nativeImage,
-  BrowserWindow,
-  MenuItemConstructorOptions
-} from 'electron';
+import { app, nativeImage, BrowserWindow } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
 let mainWindow: Electron.BrowserWindow | null;
 
-function createWindow() {
+const createWindow = () => {
   const icon = nativeImage.createFromPath(`${app.getAppPath()}/build/icon.png`);
 
   if (app.dock) {
@@ -31,6 +24,7 @@ function createWindow() {
     autoHideMenuBar: true,
     backgroundColor: '#1e1e1e',
     webPreferences: {
+      devTools: process.env.NODE_ENV === 'development',
       nodeIntegration: true
     }
   });
@@ -47,52 +41,13 @@ function createWindow() {
     );
   }
 
-  mainWindow.on('close', () => {
-    /* setWindowBounds(mainWindow?.getBounds()) */
-  });
-
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
-}
-
-async function createMenu() {
-  const template: MenuItemConstructorOptions[] = [
-    {
-      label: 'Generator',
-      submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
-    },
-    {
-      label: 'Help',
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: () => {
-            shell.openExternal('https://github.com/lvsouza/generator');
-          }
-        }
-      ]
-    }
-  ];
-
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-}
+};
 
 app.on('ready', () => {
   createWindow();
-  createMenu();
 });
 
 app.allowRendererProcessReuse = true;

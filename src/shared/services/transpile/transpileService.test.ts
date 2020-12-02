c57a00e7100ce7bbfa5b1088e41319218f62552e
lvargas <lvargas@gvdasa.com.br>
2020-12-02 07:24:07 -0300

    test: updated tests to pass

--- src/shared/services/transpile/transpileService.test.ts
+++ src/shared/services/transpile/transpileService.test.ts
@@ -19,34 +19,34 @@ describe('Strings transpile', () => {
   });
 
   test('Value replace with KebabCase', () => {
-    expect(transpileByPatterns('key: "$KebabCase({{Entidade}})",', patterns)).toBe('key: "grupo-de-itens",');
+    expect(transpileByPatterns('key: "$KebabCase<({{Entidade}})>",', patterns)).toBe('key: "grupo-de-itens",');
   });
 
   test('Value replace with KebabCase', () => {
-    expect(transpileByPatterns('$KebabCase({{Entidade}})', patterns)).toBe('grupo-de-itens');
+    expect(transpileByPatterns('$KebabCase<({{Entidade}})>', patterns)).toBe('grupo-de-itens');
   });
 
   test('Value replace with KebabCase -> PascalCase', () => {
-    expect(transpileByPatterns('$PascalCase($KebabCase({{Entidade}}))', patterns)).toBe('GrupoDeItens');
+    expect(transpileByPatterns('$PascalCase<($KebabCase<({{Entidade}})>)>', patterns)).toBe('GrupoDeItens');
   });
 
   test('Value replace with KebabCase -> PascalCase -> KebabCase', () => {
-    expect(transpileByPatterns('$KebabCase($PascalCase($KebabCase({{Entidade}})))', patterns)).toBe('grupo-de-itens');
+    expect(transpileByPatterns('$KebabCase<($PascalCase<($KebabCase<({{Entidade}})>)>)>', patterns)).toBe('grupo-de-itens');
   });
 
   test('Value replace with PascalCase', () => {
-    expect(transpileByPatterns('$PascalCase({{Entidade}})', patterns)).toBe('GrupoDeItens');
+    expect(transpileByPatterns('$PascalCase<({{Entidade}})>', patterns)).toBe('GrupoDeItens');
   });
 
   test('Value replace with CamelCase', () => {
-    expect(transpileByPatterns('$CamelCase({{Entidade}})', patterns)).toBe('grupoDeItens');
+    expect(transpileByPatterns('$CamelCase<({{Entidade}})>', patterns)).toBe('grupoDeItens');
   });
 
   test('Value replace with mulple functions', () => {
-    expect(transpileByPatterns('$PascalCase($CamelCase({{Entidade}}))-$CamelCase({{Entidade}})-$KebabCase($CamelCase({{Entidade}}))', patterns)).toBe('GrupoDeItens-grupoDeItens-grupo-de-itens');
+    expect(transpileByPatterns('$PascalCase<($CamelCase<({{Entidade}})>)>-$CamelCase<({{Entidade}})>-$KebabCase<($CamelCase<({{Entidade}})>)>', patterns)).toBe('GrupoDeItens-grupoDeItens-grupo-de-itens');
   });
 
   test('Value replace with CamelCase', () => {
-    expect(transpileByPatterns('fsdfs5$lxckjhl$$$$CamelCase({{Entidade}})213213213223333$12132323231', patterns)).toBe('fsdfs5$lxckjhl$$$grupoDeItens213213213223333$12132323231');
+    expect(transpileByPatterns('fsdfs5$lxckjhl$$$$CamelCase<({{Entidade}})>213213213223333$12132323231', patterns)).toBe('fsdfs5$lxckjhl$$$grupoDeItens213213213223333$12132323231');
   });
 });
